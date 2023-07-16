d82bcd34b47d3b3fd390e10ea58c248b6f18ae1b
Pavel Sokolov <pavlik.sokolov.2000@gmail.com>
2023-07-16 18:41:03 +0300

    feat: Changed structure folders

---
+++ src/lib/components/config-viewer/ConfigViewer.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import type { Optional } from '$lib/interfaces';
+	import { prettyJSON } from '$lib/utils';
+
+	export let json: Record<string, unknown>;
+	export let prefix: Optional<string> = '';
+	export let filename: Optional<string> = undefined;
+
+	let content = `${prefix}${prettyJSON(json)}`;
+</script>
+
+<figure class="config">
+	{#if !!filename}
+		<figcaption class="config__filename">{filename}</figcaption>
+	{/if}
+	<div class="config__code-wrapper">
+		<code class="config__code" contenteditable="false" bind:innerHTML={content} />
+	</div>
+</figure>
+
+<style lang="scss">
+	.config {
+		&__filename {
+			margin-bottom: 12px;
+		}
+
+		&__code-wrapper {
+			padding: 16px;
+			border-radius: 8px;
+			background-color: rgb(42, 42, 42);
+		}
+
+		&__code {
+			color: white;
+			outline: none;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
-	import { ConfigViewer } from '$components';
+	import { ConfigViewer } from '$lib/components';
 
-	const json = { a: 1, b: 2, foo: { c: 1, d: [] } };
+	const config = {
+		entry: "path.resolve(__dirname, 'src/index.js')",
+		output: { path: "path.resolve(__dirname, 'src/dist')", filename: 'bundle.js' }
+	};
 </script>
+
+<header>
+	<h1>Webpack configurator</h1>
+</header>
 
 <main>
 	<div class="container">
 		<section>configurator</section>
 		<section>
-			<ConfigViewer prefix="module.export = " {json} />
+			<ConfigViewer prefix="module.export = " json={config} filename="webpack.config.js" />
 		</section>
 	</div>
 </main>
